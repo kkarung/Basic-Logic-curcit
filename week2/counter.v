d721bf7320389a88ddd088251825ac21fe16e64f
kkarung <52196875+kkarung@users.noreply.github.com>
2020-10-31 14:49:35 +0900

    Create counter.v

---
+++ week2/counter.v
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------
+// N-bit Counter
+// lecture: PRACTICE IN DIGITAL LOGIC DESIGN
+// for week2 A2
+// --------------------- list ---------------------
+// N-bit Counter
+// ----------------------------------------------------------
+
+// ------------------------------------------------
+// N-bit Counter
+// ------------------------------------------------
+module counter (	cnt, clk, rst_n	);
+
+parameter		N = 2	;
+
+output	[N-1:0]		cnt	;
+input			clk	;
+input			rst_n	;
+
+reg	[N-1:0]		cnt	;
+
+always @(posedge clk or negedge rst_n) begin
+	if (rst_n == 1'b0) begin
+		cnt	 <= 0	;
+	end else begin
+		cnt	<= cnt+1;
+	end
+end
+
+endmodule
